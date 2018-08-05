4429e85844dadc04d250b9007626d28d5bb2f6df
jairo muñoz <jjms95@hotmail.com>
2018-08-05 10:33:15 -0500

    Created dropdown menu but is not complete, the cards of the index are working.

---
+++ routes/youtube.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import * as config from '../config.json';
+import { YoutubeService } from '../services/youtube';
+
+const router = express.Router();
+const service = new YoutubeService();
+
+/* GET home page. */
+router.get('/', async (req, res) => {
+	const code = req.url.replace(/^.*\//g, '').replace(/^.*\..*/g, '');
+	console.log(code, "meeeeeeeee");
+	const trends = await service.getTrendingVideos(code);
+	res.render('youtube/index', {
+		title: config.title,
+		videos: trends,
+		countries: config.countryList,
+	});
+});
+
+router.get('/:videoId', async (req, res) => {
+	res.render('youtube/player', {
+		title: config.title,
+		countries: config.countryList,
+	});
+});
+
+module.exports = router;
